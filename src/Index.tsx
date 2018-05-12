230753160c6919385731dc01ae4ac688ea449ca4
daniel.derevjanik <daniel.derevjanik@gmail.com>
2018-05-12 11:53:56 +0200

    Updated example

--- src/Index.tsx
+++ src/Index.tsx
@@ -11,6 +11,9 @@ class App extends React.Component<{}, { file: string; folder: string; ofolder: s
 
   render() {
     const { state, setState } = this;
+    const iconFileName = getIconForFile(state.file);
+    const iconFolderName = getIconForFolder(state.folder);
+    const iconOpenFolderName = getIconForOpenFolder(state.ofolder);
     return (
       <div>
         <h2>vscode-icons-js example</h2>
@@ -18,18 +21,13 @@ class App extends React.Component<{}, { file: string; folder: string; ofolder: s
         <div style={{ padding: '10px' }}>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             <div className="row">
-              Filename: <input type="text" onChange={e => this.setState({ file: e.target.value })} value={state.file} />
-              <img src={'./icons/' + getIconForFile(state.file)} alt="file" width="24" />
+              getIconForFile(<input type="text" onChange={e => this.setState({ file: e.target.value })} value={state.file} />) => <img src={'./icons/' + iconFileName} alt="file" width="24" /> <i>{iconFileName}</i>
             </div>
             <div className="row">
-              Folder:{' '}
-              <input type="text" onChange={e => this.setState({ folder: e.target.value })} value={state.folder} />
-              <img src={'./icons/' + getIconForFolder(state.folder)} alt="folder" width="24" />
+              getIconForFolder(<input type="text" onChange={e => this.setState({ folder: e.target.value })} value={state.folder} />) => <img src={'./icons/' + iconFolderName} alt="folder" width="24" /> <i>{iconFolderName}</i>
             </div>
             <div className="row">
-              Opened Folder:{' '}
-              <input type="text" onChange={e => this.setState({ ofolder: e.target.value })} value={state.ofolder} />
-              <img src={'./icons/' + getIconForOpenFolder(state.ofolder)} alt="opened folder" width="24" />
+              getIconForOpenFolder(<input type="text" onChange={e => this.setState({ ofolder: e.target.value })} value={state.ofolder} />) => <img src={'./icons/' + iconOpenFolderName} alt="opened folder" width="24" /> <i>{iconOpenFolderName}</i>
             </div>
           </div>
         </div>
